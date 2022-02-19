be87574e285046735bc9117be55bf47687696a0c
karghoff-e <karghoff.e@gmail.com>
2022-02-19 11:54:21 +0200

    Upload replays to beatleader server

--- 7_Utils/FileManager.cs
+++ 7_Utils/FileManager.cs
@@ -1,38 +1,61 @@
 ﻿using BeatLeader.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace BeatLeader.Utils
 {
     class FileManager
     {
+        private static readonly string replayFolderPath = Environment.CurrentDirectory + "\\UserData\\BeatLeader\\Replays\\";
+
+        static FileManager()
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(replayFolderPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(replayFolderPath));
+            }
+        }
+
         public static void WriteReplay(Replay replay)
         {
-            var folderPath = Environment.CurrentDirectory + "\\UserData\\BeatLeader\\Replays\\";
-            Directory.CreateDirectory(Path.GetDirectoryName(folderPath));
-
-            string filename = replay.info.playerID + (replay.info.speed != 0 ? "-practice" : "") + (replay.info.failTime != 0 ? "-fail" : "") + "-" + replay.info.songName + "-" + replay.info.difficulty + "-" + replay.info.mode + "-" + replay.info.hash + ".bsor";
-            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
-            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
-            filename = folderPath + r.Replace(filename, "_");
-            if (File.Exists(filename)) {
-                Stream stream = File.Open(filename, FileMode.Open);
-                int arrayLength = (int)stream.Length;
-                byte[] buffer = new byte[arrayLength];
-                stream.Read(buffer, 0, arrayLength);
-                stream.Close();
-
-                if (ReplayDecoder.Decode(buffer).info.score > replay.info.score) return;
-            }
-            using BinaryWriter file = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate), Encoding.UTF8);
-
+            string filename = ToFileName(replay);
+            using BinaryWriter file = new(File.Open(filename, FileMode.OpenOrCreate), Encoding.UTF8);
             ReplayEncoder.Encode(replay, file);
             file.Close();
+        }
+
+        public static string ToFileName(Replay replay)
+        {
+            string practice = replay.info.speed != 0 ? "-practice" : "";
+            string fail = replay.info.failTime != 0 ? "-fail" : "";
+            string filename = $"{replay.info.playerID}{practice}{fail}-{replay.info.songName}-{replay.info.difficulty}-{replay.info.mode}-{replay.info.hash}.bsor";
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new (string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return replayFolderPath + r.Replace(filename, "_");
+        }
+
+        public static Replay ReadReplay(string filename)
+        {
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    Stream stream = File.Open(filename, FileMode.Open);
+                    int arrayLength = (int)stream.Length;
+                    byte[] buffer = new byte[arrayLength];
+                    stream.Read(buffer, 0, arrayLength);
+                    stream.Close();
+
+                    return ReplayDecoder.Decode(buffer);
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.Debug(e);
+            }
+            return null;
         }
     }
 }
---
+++ 7_Utils/ScoreUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BeatLeader.Models;
+
+namespace BeatLeader.Utils
+{
+    class ScoreUtil
+    {
+        public static void ProcessReplay(Replay replay) 
+        {
+            if (replay.info.score <= 0) return; // no lightshow here
+
+            bool practice = replay.info.speed != 0;
+            bool fail = replay.info.failTime > 0;
+
+            if (practice || fail) {
+                FileManager.WriteReplay(replay); // save the last replay
+                return;
+            }
+
+            var localReplay = FileManager.ReadReplay(FileManager.ToFileName(replay));
+            int localHighScore = localReplay == null ? int.MinValue : localReplay.info.score;
+
+            // int serverHighScore = TODO
+
+            if (localHighScore < replay.info.score) {
+                FileManager.WriteReplay(replay);
+                UploadManager.UploadReplay(replay);
+            }
+        }
+    }
+}

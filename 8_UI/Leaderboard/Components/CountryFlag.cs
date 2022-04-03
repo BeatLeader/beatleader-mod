b99cc6394ad2cb3c1383cd04427e79641c68bd22
Reezonate <xxxx359xxxx@gmail.com>
2022-04-03 09:33:41 +0900

    Added player info

---
+++ 8_UI/Leaderboard/Components/CountryFlag.cs
@@ -0,0 +1,56 @@
+using BeatSaberMarkupLanguage.Attributes;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatLeader.Components {
+    [ViewDefinition(Plugin.ResourcesPath + ".BSML.Components.CountryFlag.bsml")]
+    internal class CountryFlag : ReeUIComponent {
+        #region Events
+
+        protected override void OnActivate(bool firstTime) {
+            if (_country != null) UpdateImage();
+        }
+
+        protected override void OnDeactivate() {
+            StopAllCoroutines();
+        }
+
+        #endregion
+
+        #region SetCountry
+
+        private string _country;
+
+        public void SetCountry(string country) {
+            _country = country;
+            if (IsActive) UpdateImage();
+        }
+
+        private void UpdateImage() {
+            var loadTask = CountryFlagsStorage.GetCountryFlagCoroutine(_country, false, OnLoadSuccess, OnLoadFailed);
+            StartCoroutine(loadTask);
+        }
+
+        private void OnLoadSuccess(Sprite sprite) {
+            SetSprite(sprite);
+        }
+
+        private void OnLoadFailed(string reason) {
+            SetSprite(BundleLoader.LocationIcon);
+        }
+
+        #endregion
+
+        #region Image
+
+        [UIComponent("image-component"), UsedImplicitly]
+        private Image _image;
+
+        private void SetSprite(Sprite sprite) {
+            _image.sprite = sprite;
+        }
+
+        #endregion
+    }
+}
--- 8_UI/Leaderboard/Components/PlayerInfo.cs
+++ 8_UI/Leaderboard/Components/PlayerInfo.cs
@@ -30,31 +30,108 @@ namespace BeatLeader.Components {
         #region Events
 
         private void OnProfileRequestStarted() {
-            PlaceholderText = "Loading...";
+            NameText = "Loading...";
+            StatsActive = false;
         }
 
-        private void OnProfileFetched(Player p) {
-            if (p == null) {
-                PlaceholderText = "Internet error or profile does not exist.";
+        private void OnProfileFetched(Player player) {
+            if (player == null) {
+                NameText = "Error";
+                StatsActive = false;
             } else {
-                _avatar.SetAvatar(p.avatar);
-                var txt = $"#{p.rank} ({p.country} #{p.countryRank}) {p.name}\n{p.pp:.00}pp";
-                PlaceholderText = txt;
+                _countryFlag.SetCountry(player.country);
+                _avatar.SetAvatar(player.avatar);
+                NameText = FormatUtils.FormatUserName(player.name);
+                GlobalRankText = FormatUtils.FormatRank(player.rank, true);
+                CountryRankText = FormatUtils.FormatRank(player.countryRank, true);
+                PpText = FormatUtils.FormatPP(player.pp);
+                StatsActive = true;
             }
         }
 
         #endregion
 
-        #region PlaceholderText
+        #region CountryRankImage
 
-        private string _placeholderText = "WOW, Such panel!";
+        [UIValue("country-flag"), UsedImplicitly]
+        private CountryFlag _countryFlag = Instantiate<CountryFlag>();
 
-        [UIValue("placeholder-text"), UsedImplicitly]
-        public string PlaceholderText {
-            get => _placeholderText;
+        #endregion
+
+        #region StatsActive
+
+        private bool _statsActive;
+
+        [UIValue("stats-active"), UsedImplicitly]
+        public bool StatsActive {
+            get => _statsActive;
             set {
-                if (_placeholderText.Equals(value)) return;
-                _placeholderText = value;
+                if (_statsActive.Equals(value)) return;
+                _statsActive = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region NameText
+
+        private string _nameText = "";
+
+        [UIValue("name-text"), UsedImplicitly]
+        public string NameText {
+            get => _nameText;
+            set {
+                if (_nameText.Equals(value)) return;
+                _nameText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region GlobalRankText
+
+        private string _globalRankText = "";
+
+        [UIValue("global-rank-text"), UsedImplicitly]
+        public string GlobalRankText {
+            get => _globalRankText;
+            set {
+                if (_globalRankText.Equals(value)) return;
+                _globalRankText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region CountryRankText
+
+        private string _countryRankText = "";
+
+        [UIValue("country-rank-text"), UsedImplicitly]
+        public string CountryRankText {
+            get => _countryRankText;
+            set {
+                if (_countryRankText.Equals(value)) return;
+                _countryRankText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PpText
+
+        private string _ppText = "";
+
+        [UIValue("pp-text"), UsedImplicitly]
+        public string PpText {
+            get => _ppText;
+            set {
+                if (_ppText.Equals(value)) return;
+                _ppText = value;
                 NotifyPropertyChanged();
             }
         }

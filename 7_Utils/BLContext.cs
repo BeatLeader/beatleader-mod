b6c0e84a14298d1eaf9b7548c52bb087dbaa3c2b
NSGolova <golova@golova.dev>
2022-04-15 15:38:28 +0100

    Made steam ticket lazy
    (cherry picked from commit 8495c2221a9ace8cceb087a64731fac2b9da3db6)

--- 7_Utils/BLContext.cs
+++ 7_Utils/BLContext.cs
@@ -5,7 +5,6 @@ namespace BeatLeader.Utils {
     // TODO: Replace by smth not static to store app context
     internal static class BLContext {
         public static Player? profile;
-        public static string steamAuthToken;
 
         public static bool IsCurrentPlayer(this Player player) {
             return string.Equals(player.id, profile?.id, StringComparison.Ordinal);

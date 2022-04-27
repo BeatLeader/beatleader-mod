0c76ad360f1af449454903e565fd51e7762864b8
Viktor Radulov <golova@golova.dev>
2022-04-27 13:21:11 +0100

    Merge pull request #17 from Reezonate
    Ree
    
    (cherry picked from commit b860840105b69fb750a10b7b3c29fa533a74f17b)

---
+++ 0_Harmony/UIPatches/HideScoreModalOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using BeatLeader.Manager;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace BeatLeader.UIPatches {
+    [HarmonyPatch(typeof(GameScenesManager), "ScenesTransitionCoroutine")]
+    internal static class HideScoreModalOnPlay {
+        [UsedImplicitly]
+        private static void Prefix() {
+            LeaderboardEvents.NotifySceneTransitionStarted();
+        }
+    }
+}
--- 8_UI/Leaderboard/Components/ScoreInfoPanel/AccuracyGraphPanel.cs
+++ 8_UI/Leaderboard/Components/ScoreInfoPanel/AccuracyGraphPanel.cs
@@ -1,33 +1,135 @@
 using System;
+using System.Linq;
 using BeatLeader.Models;
 using BeatSaberMarkupLanguage.Attributes;
 using HMUI;
 using JetBrains.Annotations;
 using UnityEngine;
+using VRUIControls;
 using Object = UnityEngine.Object;
+using Vector3 = UnityEngine.Vector3;
 
 namespace BeatLeader.Components {
-    [ViewDefinition(Plugin.ResourcesPath + ".BSML.Components.ScoreInfoPanel.AccuracyGraph.bsml")]
+    [ViewDefinition(Plugin.ResourcesPath + ".BSML.Components.ScoreInfoPanel.AccuracyGraphPanel.bsml")]
     internal class AccuracyGraphPanel : ReeUIComponent {
         #region Initialize
 
-        private AccuracyGraph _accuracyGraph;    
-        private readonly CurvedCanvasSettingsHelper _curvedCanvasSettingsHelper = new();
+        private AccuracyGraph _accuracyGraph;
 
         protected override void OnInitialize() {
             var go = Object.Instantiate(BundleLoader.AccuracyGraphPrefab, _graphContainer, true);
             _accuracyGraph = go.GetComponent<AccuracyGraph>();
+            _accuracyGraph.Construct(_graphBackground);
         }
 
         #endregion
 
         #region SetScoreStats
 
+        private float[] _points = Array.Empty<float>();
+        private float _songDuration = 1.0f;
+        private Rect _viewRect = Rect.zero;
+
         public void SetScoreStats(ScoreStats scoreStats) {
-            var tracker = scoreStats.scoreGraphTracker;
+            _songDuration = scoreStats.winTracker.endTime;
+            _points = scoreStats.scoreGraphTracker.graph;
+
+            AccuracyGraphUtils.PostProcessPoints(_points, out var positions, out _viewRect);
+            _accuracyGraph.Setup(positions, _viewRect, GetCanvasRadius(), _songDuration);
+        }
+
+        #endregion
+
+        #region CursorPosition
+
+        private readonly Vector3[] _corners = new Vector3[4];
+        private VRPointer _vrPointer;
+        private Vector3 _lastPosition3D;
+        private bool _cursorInitialized;
+
+        protected override void OnActivate(bool firstTime) {
+            _vrPointer = FindObjectOfType<VRPointer>();
+            _cursorInitialized = _vrPointer != null;
+            _lastPosition3D = default;
+        }
+
+        private void LateUpdate() {
+            if (!_cursorInitialized) return;
+
+            var cursorPosition3D = _vrPointer.cursorPosition;
+            if (cursorPosition3D.Equals(_lastPosition3D)) return;
+            _lastPosition3D = cursorPosition3D;
+
+            CalculateCursorPosition(cursorPosition3D, out var normalized);
+            UpdateCursor(normalized);
+        }
+
+        private void CalculateCursorPosition(Vector3 worldCursor, out Vector2 normalized) {
+            var canvasRadius = GetCanvasRadius() * _graphContainer.lossyScale.x;
+            var nonCurved = AccuracyGraphUtils.TransformPointFrom3DToCanvas(worldCursor, canvasRadius);
+
+            _graphContainer.GetWorldCorners(_corners);
+
+            normalized = new Vector2(
+                new Range(_corners[0].x, _corners[3].x).GetRatio(nonCurved.x),
+                new Range(_corners[0].y, _corners[1].y).GetRatio(nonCurved.y)
+            );
+        }
+
+        #endregion
+
+        #region UpdateCursor
+
+        private float _targetViewTime;
+        private float _currentViewTime;
+
+        private void UpdateCursor(Vector2 normalized) {
+            if (normalized.x < 0 || normalized.y < 0 || normalized.x > 1 || normalized.y > 1) return;
+            var viewCursor = Rect.NormalizedToPoint(_viewRect, normalized);
+            _targetViewTime = Mathf.Clamp01(viewCursor.x);
+        }
+
+        private void Update() {
+            if (float.IsNaN(_targetViewTime)) return;
+            _currentViewTime = Mathf.Lerp(_currentViewTime, _targetViewTime, Time.deltaTime * 10.0f);
+            var songTime = _currentViewTime * _songDuration;
+            var accuracy = GetAccuracy(_currentViewTime);
+            _accuracyGraph.SetCursor(_currentViewTime);
+            CursorText = FormatCursorText(songTime, accuracy);
+        }
+
+        private static string FormatCursorText(float songTime, float accuracy) {
+            var fullMinutes = Mathf.FloorToInt(songTime / 60.0f);
+            var remainingSeconds = Mathf.RoundToInt(Mathf.Abs(songTime % 60.0f));
+            return $"<color=#B856FF>time: </color>{fullMinutes}:{remainingSeconds:00}  <color=#B856FF>accuracy: </color>{accuracy * 100.0f:F2}<size=70%>%";
+        }
+
+        private float GetAccuracy(float viewTime) {
+            if (_points.Length == 0) return 1.0f;
             
-            var canvasSettings = _curvedCanvasSettingsHelper.GetCurvedCanvasSettings(_accuracyGraph.canvas);
-            _accuracyGraph.SetPoints(tracker.graph, canvasSettings.radius);
+            var xStep = 1.0f / _points.Length;
+            var x = xStep;
+            
+            for (var i = 1; i < _points.Length; i++, x += xStep) {
+                if (x < viewTime) continue;
+                var xRange = new Range(x - xStep, x);
+                var yRange = new Range(_points[i - 1], _points[i]);
+                var ratio = xRange.GetRatio(viewTime);
+                return yRange.SlideBy(ratio);
+            }
+
+            return _points.Last();
+        }
+
+        #endregion
+
+        #region GetCanvasRadius
+
+        private readonly CurvedCanvasSettingsHelper _curvedCanvasSettingsHelper = new();
+
+        private float GetCanvasRadius() {
+            var canvasSettings = _curvedCanvasSettingsHelper.GetCurvedCanvasSettings(_accuracyGraph.Canvas);
+            return canvasSettings == null ? float.MaxValue : canvasSettings.radius;
         }
 
         #endregion
@@ -56,10 +158,32 @@ namespace BeatLeader.Components {
 
         #endregion
 
-        #region GraphContainer
+        #region UIComponents
 
         [UIComponent("graph-container"), UsedImplicitly]
         private RectTransform _graphContainer;
+
+        [UIComponent("graph-container"), UsedImplicitly]
+        private ImageView _graphBackground;
+
+        [UIComponent("cursor-hint"), UsedImplicitly]
+        private RectTransform _hintTransform;
+
+        #endregion
+
+        #region CursorText
+
+        private string _cursorText = "";
+
+        [UIValue("cursor-text"), UsedImplicitly]
+        private string CursorText {
+            get => _cursorText;
+            set {
+                if (_cursorText.Equals(value)) return;
+                _cursorText = value;
+                NotifyPropertyChanged();
+            }
+        }
 
         #endregion
     }

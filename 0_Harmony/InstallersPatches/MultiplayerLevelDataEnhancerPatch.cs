d087400246edee4adda2e20b112bc7380fc6dfe8
karghoff-e <karghoff.e@gmail.com>
2022-07-01 18:47:44 +0300

    Score submission for multiplayer
    - added MP specific classes
    - multiplayer game mode is now allowed
    - initiate recorder in MP only if
    -- a player is not a spectator
    -- a player pressed start/ready button
    -- a lobby owner started a countdown timer
    
    - http errors in range [400-450] will not trigger a retry for score upload
    - fixed injection for PauseController in replay recorder as it is optional for MP

---
+++ 0_Harmony/InstallersPatches/MultiplayerLevelDataEnhancerPatch.cs
@@ -0,0 +1,23 @@
+﻿using BeatLeader.Core.Managers.ReplayEnhancer;
+using HarmonyLib;
+
+namespace BeatLeader {
+    [HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), nameof(MultiplayerLevelScenesTransitionSetupDataSO.Init))]
+    class MultiplayerLevelDataEnhancerPatch
+    {
+        static void Postfix(MultiplayerLevelScenesTransitionSetupDataSO __instance, IPreviewBeatmapLevel previewBeatmapLevel, IDifficultyBeatmap difficultyBeatmap, ColorScheme overrideColorScheme,
+            GameplayModifiers gameplayModifiers, PlayerSpecificSettings playerSpecificSettings, PracticeSettings practiceSettings, bool useTestNoteCutSoundEffects = false)
+        {
+            Plugin.Log.Debug("MultiplayerLevelDataEnhancerPatch.postfix");
+            EnvironmentInfoSO environmentInfoSO = difficultyBeatmap.GetEnvironmentInfo();
+            MapEnhancer.difficultyBeatmap = difficultyBeatmap;
+            MapEnhancer.previewBeatmapLevel = previewBeatmapLevel;
+            MapEnhancer.gameplayModifiers = gameplayModifiers;
+            MapEnhancer.playerSpecificSettings = playerSpecificSettings;
+            MapEnhancer.practiceSettings = practiceSettings;
+            MapEnhancer.useTestNoteCutSoundEffects = useTestNoteCutSoundEffects;
+            MapEnhancer.environmentInfo = environmentInfoSO;
+            MapEnhancer.colorScheme = overrideColorScheme;
+        }
+    }
+}
--- 2_Core/Installers/OnGameplayCoreInstaller.cs
+++ 2_Core/Installers/OnGameplayCoreInstaller.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using BeatLeader.Core.Managers.ReplayEnhancer;
 using BeatLeader.Utils;
@@ -8,6 +9,9 @@ using Zenject;
 namespace BeatLeader.Installers {
     [UsedImplicitly]
     public class OnGameplayCoreInstaller : Installer<OnGameplayCoreInstaller> {
+
+        private List<string> modes = new() { "Solo", "Multiplayer" };
+
         public override void InstallBindings() {
             Plugin.Log.Debug("OnGameplayCoreInstaller");
 
@@ -29,8 +33,9 @@ namespace BeatLeader.Installers {
                     Plugin.Log.Debug("OST level detected. No recording.");
                     return;
                 }
-                if (GetSetupDataSO()?.gameMode != "Solo") {
-                    Plugin.Log.Debug("Not a \"Solo\" game mode");
+                var gameMode = GameMode();
+                if (gameMode != null && !modes.Contains(gameMode)) {
+                    Plugin.Log.Debug("Not allowed game mode");
                     return;
                 }
                 #endregion
@@ -44,13 +49,38 @@ namespace BeatLeader.Installers {
             }
         }
 
-        private StandardLevelScenesTransitionSetupDataSO GetSetupDataSO() {
+        #region Game mode stuff
+
+        private string GameMode() {
+            string singleGM = GetStandardDataSO()?.gameMode;
+            string multiGM = GetMultiDataSO()?.gameMode;
+
+            foreach (string mode in new[] { singleGM, multiGM }) {
+                if (mode != null && mode.Length > 0) {
+                    return mode;
+                }
+            }
+
+            return null;
+        }
+
+        private StandardLevelScenesTransitionSetupDataSO GetStandardDataSO() {
             try {
                 return Container.TryResolve<StandardLevelScenesTransitionSetupDataSO>();
             } catch {
                 return null;
             }
         }
+
+        private MultiplayerLevelScenesTransitionSetupDataSO GetMultiDataSO() {
+            try {
+                return Container.TryResolve<MultiplayerLevelScenesTransitionSetupDataSO>();
+            } catch {
+                return null;
+            }
+        }
+
+        #endregion
 
         // TODO: remove this after verifying of an "allowed flows" strategy
         private static readonly MethodBase ScoreSaber_playbackEnabled = AccessTools.Method("ScoreSaber.Core.ReplaySystem.HarmonyPatches.PatchHandleHMDUnmounted:Prefix");

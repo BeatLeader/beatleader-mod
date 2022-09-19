be96467361a3e880c8c97a93ac8e4c90c6b3a560
Hermanest <nicedevelooper@gmail.com>
2022-09-19 17:29:51 +0300

    more refactoring

--- 1_Config/AutomaticConfigTool.cs
+++ 1_Config/AutomaticConfigTool.cs
@@ -2,14 +2,10 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 using BeatLeader.Utils;
 using Newtonsoft.Json;
 using System.Reflection;
 using Newtonsoft.Json.Linq;
-using System.Runtime.Serialization;
 
 namespace BeatLeader
 {

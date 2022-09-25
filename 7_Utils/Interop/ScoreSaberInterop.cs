e3d90907b9e090b2e6c6478d5f7be9019a9a778f
Hermanest <nicedevelooper@gmail.com>
2022-09-25 19:31:45 +0300

    scoresaber interop rework

--- 7_Utils/Interop/ScoreSaberInterop.cs
+++ 7_Utils/Interop/ScoreSaberInterop.cs
@@ -1,19 +1,15 @@
 ﻿using BeatLeader.Utils;
 using IPA.Loader;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using Zenject;
 
 namespace BeatLeader.Interop
 {
     internal static class ScoreSaberInterop
     {
         private static Assembly _assembly;
-        private static Type _installerType1;
-        private static Type _installerType2;
-        private static Type _installerType3;
-        private static MethodInfo _installMethodInfo1;
-        private static MethodInfo _installMethodInfo2;
-        private static MethodInfo _installMethodInfo3;
         private static HarmonyMultisilencer _installatorsSilencer;
 
         public static bool RecordingEnabled
@@ -26,7 +22,6 @@ namespace BeatLeader.Interop
             }
         }
 
-        //UAHAHAHAH, DIE SOSABER
         public static void Init()
         {
             _assembly = PluginManager.GetPluginFromId("ScoreSaber")?.Assembly;
@@ -34,13 +29,21 @@ namespace BeatLeader.Interop
 
             try
             {
-                _installerType1 = _assembly.GetType("#=zX_v74G3PzI7aCZIakETp3Jix2zF4LcMlbuBZjFrpZV7rLX_AyQ==");
-                _installerType2 = _assembly.GetType("#=z04iHj80ruN9mJYvYhK1kO8nt_cId_4J1Mi_w_iWR2Ltr");
-                _installerType3 = _assembly.GetType("#=zSiVtl_ZozGZMq$Qlhd4LwcTOeQbdl3_dta0EEVJIT8xJ");
-                _installMethodInfo1 = _installerType1.GetMethod("InstallBindings", ReflectionUtils.DefaultFlags);
-                _installMethodInfo2 = _installerType2.GetMethod("InstallBindings", ReflectionUtils.DefaultFlags);
-                _installMethodInfo3 = _installerType3.GetMethod("InstallBindings", ReflectionUtils.DefaultFlags);
-                _installatorsSilencer = new(new[] { _installMethodInfo1, _installMethodInfo2, _installMethodInfo3 }, false);
+                var types = _assembly.GetTypes();
+                var methods = new List<MethodInfo>();
+
+                foreach (var item in types)
+                {
+                    if (item.IsSubclassOf(typeof(Installer)))
+                    {
+                        var method = item.GetMethod(nameof(
+                            Installer.InstallBindings), ReflectionUtils.DefaultFlags);
+                        methods.Add(method);
+                    }
+                }
+
+                Plugin.Log.Info($"Found {methods.Count} ScoreSaber installators");
+                _installatorsSilencer = new(methods, false);
             }
             catch
             {

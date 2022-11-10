5e64bb6565cfb706a94bcedfa13da33fb3d03d47
NSGolova <golova@golova.dev>
2022-11-10 11:13:43 +0000

    Use Steam token from the game

--- 2_Core/API/Authentication.cs
+++ 2_Core/API/Authentication.cs
@@ -37,26 +37,7 @@ namespace BeatLeader.API {
         }
 
         public static async Task<string> SteamTicket() {
-            if (!SteamManager.Initialized) {
-                Plugin.Log.Error("SteamManager is not initialized!");
-                return null;
-            }
-
-            var getAuthResponseTcs = new TaskCompletionSource<bool>();
-            var getAuthSessionTicketCallback = new Callback<GetAuthSessionTicketResponse_t>(resp => getAuthResponseTcs.TrySetResult(resp.m_eResult == EResult.k_EResultOK));
-
-            string ticket = null;
-
-            const int ticketBufferSize = 1024;
-            var byteBuffer = new byte[ticketBufferSize];
-            if (SteamUser.GetAuthSessionTicket(byteBuffer, ticketBufferSize, out var ticketLength) != HAuthTicket.Invalid) {
-                ticket = BitConverter.ToString(byteBuffer, 0, (int)ticketLength).Replace("-", "");
-            }
-
-            await Task.WhenAny(getAuthResponseTcs.Task, Task.Delay(TimeSpan.FromSeconds(10.0))).ConfigureAwait(false);
-
-            getAuthSessionTicketCallback.Dispose();
-            return !getAuthResponseTcs.Task.IsCompleted || !getAuthResponseTcs.Task.Result ? null : ticket;
+            return (await new SteamPlatformUserModel().GetUserAuthToken()).token;
         }
 
         public static async Task<string> OculusTicket() {

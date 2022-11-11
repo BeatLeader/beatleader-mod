8e8dc0dd8ed0500f58af730a62c2e9b3d7ed03e5
Reezonate <xxxx359xxxx@gmail.com>
2022-11-11 23:01:28 +0900

    Leaderboard pre-parsers

--- 8_UI/Leaderboard/ViewControllers/LeaderboardPanel.cs
+++ 8_UI/Leaderboard/ViewControllers/LeaderboardPanel.cs
@@ -2,25 +2,46 @@ using BeatLeader.Components;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.ViewControllers;
 using JetBrains.Annotations;
+using UnityEngine;
+using Zenject;
 
 namespace BeatLeader.ViewControllers {
     [ViewDefinition(Plugin.ResourcesPath + ".BSML.Leaderboard.LeaderboardPanel.bsml")]
     internal class LeaderboardPanel : BSMLAutomaticViewController {
+        #region PreParser
+
+        [Inject, UsedImplicitly]
+        private PreParser _preParser;
+
+        public class PreParser : MonoBehaviour {
+            public StatusBar statusBar;
+            public Logo logo;
+            public PlayerInfo playerInfo;
+
+            private void Awake() {
+                statusBar = ReeUIComponentV2.InstantiateOnSceneRoot<StatusBar>();
+                logo = ReeUIComponentV2.InstantiateOnSceneRoot<Logo>();
+                playerInfo = ReeUIComponentV2.InstantiateOnSceneRoot<PlayerInfo>();
+            }
+        }
+
+        #endregion
+
         #region Components
 
         [UIValue("status-bar"), UsedImplicitly]
-        private StatusBar _statusBar = ReeUIComponentV2.InstantiateOnSceneRoot<StatusBar>();
+        private StatusBar StatusBar => _preParser.statusBar;
 
         [UIValue("logo"), UsedImplicitly]
-        private Logo _logo = ReeUIComponentV2.InstantiateOnSceneRoot<Logo>();
+        private Logo Logo => _preParser.logo;
 
         [UIValue("player-info"), UsedImplicitly]
-        private PlayerInfo _playerInfo = ReeUIComponentV2.InstantiateOnSceneRoot<PlayerInfo>();
+        private PlayerInfo PlayerInfo => _preParser.playerInfo;
 
         private void Awake() {
-            _statusBar.SetParent(transform);
-            _logo.SetParent(transform);
-            _playerInfo.SetParent(transform);
+            StatusBar.SetParent(transform);
+            Logo.SetParent(transform);
+            PlayerInfo.SetParent(transform);
         }
 
         #endregion
--- 8_UI/Leaderboard/ViewControllers/LeaderboardView.cs
+++ 8_UI/Leaderboard/ViewControllers/LeaderboardView.cs
@@ -2,53 +2,88 @@ using BeatLeader.Components;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.ViewControllers;
 using JetBrains.Annotations;
+using UnityEngine;
+using Zenject;
 
 namespace BeatLeader.ViewControllers {
     [ViewDefinition(Plugin.ResourcesPath + ".BSML.Leaderboard.LeaderboardView.bsml")]
     internal class LeaderboardView : BSMLAutomaticViewController {
+        #region PreParser
+
+        [Inject, UsedImplicitly]
+        private PreParser _preParser;
+
+        public class PreParser : MonoBehaviour {
+            public VotingPanel votingPanel;
+            public BeatLeaderInfo beatLeaderInfo;
+            public LeaderboardSettings leaderboardSettings;
+            public ScoreInfoPanel scoreInfoPanel;
+            public ScoresTable scoresTable;
+            public VotingButton votingButton;
+            public Pagination pagination;
+            public ScopeSelector scopeSelector;
+            public ContextSelector contextSelector;
+            public EmptyBoardMessage emptyBoardMessage;
+
+            private void Awake() {
+                votingPanel = ReeUIComponentV2.InstantiateOnSceneRoot<VotingPanel>(false);
+                beatLeaderInfo = ReeUIComponentV2.InstantiateOnSceneRoot<BeatLeaderInfo>(false);
+                leaderboardSettings = ReeUIComponentV2.InstantiateOnSceneRoot<LeaderboardSettings>(false);
+                scoreInfoPanel = ReeUIComponentV2.InstantiateOnSceneRoot<ScoreInfoPanel>(false);
+                scoresTable = ReeUIComponentV2.InstantiateOnSceneRoot<ScoresTable>();
+                votingButton = ReeUIComponentV2.InstantiateOnSceneRoot<VotingButton>(false);
+                pagination = ReeUIComponentV2.InstantiateOnSceneRoot<Pagination>(false);
+                scopeSelector = ReeUIComponentV2.InstantiateOnSceneRoot<ScopeSelector>(false);
+                contextSelector = ReeUIComponentV2.InstantiateOnSceneRoot<ContextSelector>(false);
+                emptyBoardMessage = ReeUIComponentV2.InstantiateOnSceneRoot<EmptyBoardMessage>();
+            }
+        }
+
+        #endregion
+
         #region Components
 
         [UIValue("voting-panel"), UsedImplicitly]
-        private VotingPanel _votingPanel = ReeUIComponentV2.InstantiateOnSceneRoot<VotingPanel>(false);
+        private VotingPanel VotingPanel => _preParser.votingPanel;
 
         [UIValue("beat-leader-info"), UsedImplicitly]
-        private BeatLeaderInfo _beatLeaderInfo = ReeUIComponentV2.InstantiateOnSceneRoot<BeatLeaderInfo>(false);
+        private BeatLeaderInfo BeatLeaderInfo => _preParser.beatLeaderInfo;
 
         [UIValue("leaderboard-settings"), UsedImplicitly]
-        private LeaderboardSettings _leaderboardSettings = ReeUIComponentV2.InstantiateOnSceneRoot<LeaderboardSettings>(false);
+        private LeaderboardSettings LeaderboardSettings => _preParser.leaderboardSettings;
 
         [UIValue("score-info-panel"), UsedImplicitly]
-        private ScoreInfoPanel _scoreInfoPanel = ReeUIComponentV2.InstantiateOnSceneRoot<ScoreInfoPanel>(false);
+        private ScoreInfoPanel ScoreInfoPanel => _preParser.scoreInfoPanel;
 
         [UIValue("scores-table"), UsedImplicitly]
-        private ScoresTable _scoresTable = ReeUIComponentV2.InstantiateOnSceneRoot<ScoresTable>();
+        private ScoresTable ScoresTable => _preParser.scoresTable;
 
         [UIValue("voting-button"), UsedImplicitly]
-        private VotingButton _votingButton = ReeUIComponentV2.InstantiateOnSceneRoot<VotingButton>(false);
+        private VotingButton VotingButton => _preParser.votingButton;
 
         [UIValue("pagination"), UsedImplicitly]
-        private Pagination _pagination = ReeUIComponentV2.InstantiateOnSceneRoot<Pagination>(false);
+        private Pagination Pagination => _preParser.pagination;
 
         [UIValue("scope-selector"), UsedImplicitly]
-        private ScopeSelector _scopeSelector = ReeUIComponentV2.InstantiateOnSceneRoot<ScopeSelector>(false);
+        private ScopeSelector ScopeSelector => _preParser.scopeSelector;
 
         [UIValue("context-selector"), UsedImplicitly]
-        private ContextSelector _contextSelector = ReeUIComponentV2.InstantiateOnSceneRoot<ContextSelector>(false);
+        private ContextSelector ContextSelector => _preParser.contextSelector;
 
         [UIValue("empty-board-message"), UsedImplicitly]
-        private EmptyBoardMessage _emptyBoardMessage = ReeUIComponentV2.InstantiateOnSceneRoot<EmptyBoardMessage>();
+        private EmptyBoardMessage EmptyBoardMessage => _preParser.emptyBoardMessage;
 
         private void Awake() {
-            _votingPanel.SetParent(transform);
-            _beatLeaderInfo.SetParent(transform);
-            _leaderboardSettings.SetParent(transform);
-            _scoreInfoPanel.SetParent(transform);
-            _scoresTable.SetParent(transform);
-            _votingButton.SetParent(transform);
-            _pagination.SetParent(transform);
-            _scopeSelector.SetParent(transform);
-            _contextSelector.SetParent(transform);
-            _emptyBoardMessage.SetParent(transform);
+            VotingPanel.SetParent(transform);
+            BeatLeaderInfo.SetParent(transform);
+            LeaderboardSettings.SetParent(transform);
+            ScoreInfoPanel.SetParent(transform);
+            ScoresTable.SetParent(transform);
+            VotingButton.SetParent(transform);
+            Pagination.SetParent(transform);
+            ScopeSelector.SetParent(transform);
+            ContextSelector.SetParent(transform);
+            EmptyBoardMessage.SetParent(transform);
         }
 
         #endregion

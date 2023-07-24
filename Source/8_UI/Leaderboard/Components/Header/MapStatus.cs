a0fc803514a32281a86b8dfd8df5f403b86d23a3
Hermanest <nicedevelooper@gmail.com>
2023-07-24 19:13:02 +0300

    added star rating for modifiers

--- Source/8_UI/Leaderboard/Components/Header/MapStatus.cs
+++ Source/8_UI/Leaderboard/Components/Header/MapStatus.cs
@@ -9,43 +9,74 @@ namespace BeatLeader.Components {
         #region Components
 
         [UIComponent("background"), UsedImplicitly]
-        private ImageView _background;
+        private ImageView _background = null!;
 
         [UIComponent("status-text"), UsedImplicitly]
-        private TextMeshProUGUI _statusText;
+        private TextMeshProUGUI _statusText = null!;
 
         #endregion
 
         #region Init/Dispose
 
-        private SmoothHoverController _smoothHoverController;
+        private SmoothHoverController _smoothHoverController = null!;
 
         protected override void OnInitialize() {
             _background.raycastTarget = true;
             _smoothHoverController = _background.gameObject.AddComponent<SmoothHoverController>();
             _smoothHoverController.HoverStateChangedEvent += OnHoverStateChanged;
+            GameplayModifiersPanelPatch.ModifiersChangedEvent += OnModifiersChanged;
         }
 
         protected override void OnDispose() {
             _smoothHoverController.HoverStateChangedEvent -= OnHoverStateChanged;
+            GameplayModifiersPanelPatch.ModifiersChangedEvent -= OnModifiersChanged;
         }
-
-        private void OnHoverStateChanged(bool isHovered, float progress) {
-            MapDifficultyPanel.NotifyMapStatusHoverStateChanged(_background.transform.position, isHovered, progress);
-        }
-
+        
         #endregion
 
         #region SetValues
 
+        private GameplayModifiers? _gameplayModifiers;
+        private RankedStatus _rankedStatus;
+        private DiffInfo _diffInfo;
+        
         public void SetActive(bool value) {
             _background.gameObject.SetActive(value);
             OnHoverStateChanged(false, 0.0f);
         }
 
         public void SetValues(RankedStatus rankedStatus, DiffInfo diffInfo) {
-            MapDifficultyPanel.NotifyDiffInfoChanged(diffInfo);
-            _statusText.text = diffInfo.stars > 0 ? $"{rankedStatus}: {FormatUtils.FormatStars(diffInfo.stars)}" : rankedStatus.ToString();
+            _rankedStatus = rankedStatus;
+            _diffInfo = diffInfo;
+            UpdateVisuals();
+        }
+
+        private void UpdateVisuals() {
+            MapDifficultyPanel.NotifyDiffInfoChanged(_diffInfo);
+            var stars = _diffInfo.stars;
+            if (_diffInfo.modifiersRating is { } rating &&
+                _gameplayModifiers is { songSpeed: not GameplayModifiers.SongSpeed.Normal } modifiers) {
+                stars = modifiers.songSpeed switch {
+                    GameplayModifiers.SongSpeed.Slower => rating.ssStars,
+                    GameplayModifiers.SongSpeed.Faster => rating.fsStars,
+                    GameplayModifiers.SongSpeed.SuperFast => rating.sfStars,
+                    _ => stars,
+                };
+            }
+            _statusText.text = _diffInfo.stars > 0 ? $"{_rankedStatus}: {FormatUtils.FormatStars(stars)}" : _rankedStatus.ToString();
+        }
+
+        #endregion
+
+        #region Callbacks
+        
+        private void OnHoverStateChanged(bool isHovered, float progress) {
+            MapDifficultyPanel.NotifyMapStatusHoverStateChanged(_background.transform.position, isHovered, progress);
+        }
+
+        private void OnModifiersChanged(GameplayModifiers modifiers) {
+            _gameplayModifiers = modifiers;
+            UpdateVisuals();
         }
 
         #endregion

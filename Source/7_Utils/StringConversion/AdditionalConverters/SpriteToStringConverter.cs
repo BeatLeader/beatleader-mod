0e1255f4bfa9f05e17f7ca258d43532f056bdcd3
Hermanest <nicedevelooper@gmail.com>
2023-08-08 23:19:20 +0300

    StringConverters for ReeUIComponentV3

---
+++ Source/7_Utils/StringConversion/AdditionalConverters/SpriteToStringConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BeatLeader.Utils;
+using UnityEngine;
+
+namespace BeatLeader {
+    public class SpriteToStringConverter : StringConverter<Sprite> {
+        protected override Sprite? ConvertTyped(string str) {
+            try {
+                return BSMLUtility.LoadSprite(str);
+            } catch (Exception) {
+                return null;
+            }
+        }
+    }
+}
---
+++ Source/7_Utils/StringConversion/BasicConverters/BoolToStringConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace BeatLeader {
+    public class BoolToStringConverter : StringConverter<bool?> {
+        protected override bool? ConvertTyped(string str) {
+            return bool.TryParse(str, out var value) ? value : null;
+        }
+    }
+}

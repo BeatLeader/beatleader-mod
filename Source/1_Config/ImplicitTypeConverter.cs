bf8f287e6729bc227526b316fb75dcd6fe05de39
Hermanest <nicedevelooper@gmail.com>
2023-09-24 12:59:42 +0300

    config migration (typed json converter)

---
+++ Source/1_Config/ImplicitTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BeatLeader {
+    public class ImplicitTypeConverter<T> : JsonConverter {
+        private readonly Type _targetType = typeof(T);
+        
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) {
+            serializer.Serialize(writer, value);
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) {
+            var token = JToken.Load(reader);
+            return token.ToObject<T>();
+        }
+
+        public override bool CanConvert(Type objectType) {
+            return objectType.IsAssignableFrom(_targetType);
+        }
+    }
+}

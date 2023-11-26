bc66b981d4cf64cf3d8359b370ec29b8b3c6279f
Hermanest <nicedevelooper@gmail.com>
2023-11-26 19:21:33 +0300

    added player loading to the BattleRoyaleDetailPanel

---
+++ Source/2_Core/WebRequestsV2/Requests/PlayerRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using BeatLeader.Models;
+using BeatLeader.Utils;
+using BeatLeader.WebRequests;
+
+namespace BeatLeader.API {
+    public class PlayerRequest : PersistentWebRequestWithResult<PlayerRequest, Player, JsonWebRequestDescriptor<Player>> {
+        public static IWebRequest<Player> SendRequest(string playerId) {
+            return SendRet(BLConstants.BEATLEADER_API_URL + $"/player/{playerId}", HttpMethod.Get);
+        }
+    }
+}

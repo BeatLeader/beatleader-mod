cd5a58053838e118276eed90f85f32e57a231d1b
Hermanest <nicedevelooper@gmail.com>
2023-12-02 20:17:33 +0300

    made ReeUIComponentV3's code a little bit more readable

--- Source/8_UI/UIComponentV3/GenericDescriptor/BasicUIComponentDescriptor.cs
+++ Source/8_UI/UIComponentV3/GenericDescriptor/BasicUIComponentDescriptor.cs
@@ -8,32 +8,41 @@ using IPA.Utilities;
 using UnityEngine;
 
 namespace BeatLeader.Components {
-    internal class GenericUIComponentDescriptor<T> : IUIComponentDescriptor<T> where T : class {
-        static GenericUIComponentDescriptor() {
-            var components = typeof(T)
-                .GetMembersWithAttribute<ExternalComponentAttribute, MemberInfo>();
+    internal class BasicUIComponentDescriptor<T> : IReeUIComponentDescriptor<T> where T : class {
+        #region UIComponentDescriptor
+
+        static BasicUIComponentDescriptor() {
+            var components = typeof(T).GetMembersWithAttribute<ExternalComponentAttribute, MemberInfo>();
 
             var setters = new Dictionary<string, Action<T, BSMLParserParams, object>>();
-            HandleExternalPropertySetters(GetProperties(typeof(T)), setters);
+            ProcessExternalPropertySetters(GetProperties(typeof(T)), setters);
             propertySetters = setters;
 
             var collection = new Dictionary<Type, Func<T, Component>>();
-            foreach (var (member, attribute) in components) {
-                if (!member.GetMemberTypeImplicitly(out var type)
-                    || !type!.IsSubclassOf(typeof(Component))) continue;
+            foreach (var pair in components) {
+                var member = pair.Key;
+
+                var memberTypeIsCorrect = member.GetMemberTypeImplicitly(out var type);
+                var memberSuperclassIsCorrect = type!.IsSubclassOf(typeof(Component));
+                if (!memberTypeIsCorrect || !memberSuperclassIsCorrect) continue;
+
                 if (collection.ContainsKey(type)) {
                     Plugin.Log.Warn($"Cannot add multiple components of type {type.Name} to {typeof(T).Name}!");
                     continue;
                 }
-                collection.Add(type, x => {
-                    member.GetValueImplicitly(x, out var val);
-                    return (val as Component)!;
-                });
+
+                var acquireValueDelegate = new Func<T, Component>(
+                    x => {
+                        member.GetValueImplicitly(x, out var val);
+                        return (Component)val!;
+                    }
+                );
+                collection.Add(type, acquireValueDelegate);
             }
             componentGetters = collection.Select(x => x.Value);
         }
 
-        public virtual string ComponentName { get; } = typeof(T).Name;
+        public string ComponentName { get; } = typeof(T).Name;
 
         public IDictionary<string, Action<T, BSMLParserParams, object>> ExternalProperties => propertySetters;
 
@@ -42,9 +51,11 @@ namespace BeatLeader.Components {
         private static readonly IDictionary<string, Action<T, BSMLParserParams, object>> propertySetters;
         private static readonly IEnumerable<Func<T, Component>> componentGetters;
 
+        #endregion
+
         #region Property Assigning
 
-        private static void HandleExternalPropertySetters(
+        private static void ProcessExternalPropertySetters(
             ICollection<(MemberInfo, string, ExternalPropertyAttribute)> properties,
             IDictionary<string, Action<T, BSMLParserParams, object>> dict,
             string? prefix = null,
@@ -52,38 +63,33 @@ namespace BeatLeader.Components {
         ) {
             prefix = prefix is not null ? $"{prefix}." : null;
             if (properties.Count == 0) return;
+
             foreach (var (member, propName, attribute) in properties) {
                 var name = $"{prefix}{propName}";
                 if (dict.ContainsKey(name)) {
                     throw new InvalidOperationException($"Cannot bind multiple properties with the same name \"{name}\"!");
                 }
+
                 if (attribute.ExportMode is PropertyExportMode.Inherit) {
                     member.GetMemberTypeImplicitly(out var memberType);
                     var props = GetProperties(memberType!);
-                    props = attribute.PropertiesToInherit is { } inheritProps
-                        ? props.Where(x => inheritProps.Contains(x.Item2)).ToArray()
-                        : props;
+
+                    if (attribute.PropertiesToInherit is { } inheritProps) {
+                        props = props.Where(x => inheritProps.Contains(x.Item2)).ToArray();
+                    }
+
                     var valueStack = CloneAndAppendToStack(stackMembers, member);
                     prefix = $"{prefix}{attribute.Prefix}";
-                    HandleExternalPropertySetters(props, dict, string.IsNullOrEmpty(prefix) ? null : prefix, valueStack);
+                    ProcessExternalPropertySetters(props, dict, string.IsNullOrEmpty(prefix) ? null : prefix, valueStack);
                     continue;
                 }
-                dict.Add(name, (x, parserParams, val) => SetProperty(
-                    member, AcquireValueByStack(x, stackMembers), parserParams, val));
+
+                dict.Add(
+                    name, (x, parserParams, val) => SetProperty(
+                        member, AcquireValueByStack(x, stackMembers), parserParams, val
+                    )
+                );
             }
-        }
-
-        private static Stack<MemberInfo> CloneAndAppendToStack(Stack<MemberInfo>? stack, MemberInfo memberToAppend) {
-            var newStack = new Stack<MemberInfo>(stack?.ToArray() ?? Array.Empty<MemberInfo>());
-            newStack.Push(memberToAppend);
-            return newStack;
-        }
-
-        private static object? AcquireValueByStack(object obj, IEnumerable<MemberInfo>? members) {
-            if (members is null) return obj;
-            var value = obj;
-            foreach (var member in members) member.GetValueImplicitly(value!, out value);
-            return value;
         }
 
         private static void SetProperty(MemberInfo member, object? obj, BSMLParserParams parserParams, object value) {
@@ -119,13 +125,31 @@ namespace BeatLeader.Components {
 
         private static ICollection<(MemberInfo, string, ExternalPropertyAttribute)> GetProperties(Type type) {
             return type.GetMembers(ReflectionUtils.DefaultFlags)
-                .Select(static x => {
-                    var attr = x.GetCustomAttribute<ExternalPropertyAttribute>();
-                    return (x, attr?.Name ?? x.Name, attr);
-                })
-                .Where(static x => x.attr is not null
-                    && x.x is PropertyInfo or FieldInfo or EventInfo)
+                .Select(
+                    static x => {
+                        var attr = x.GetCustomAttribute<ExternalPropertyAttribute>();
+                        return (x, attr?.Name ?? x.Name, attr);
+                    }
+                )
+                .Where(static x => x.attr is not null && x.x is PropertyInfo or FieldInfo or EventInfo)
                 .ToArray()!;
+        }
+
+        #endregion
+
+        #region Stack
+
+        private static Stack<MemberInfo> CloneAndAppendToStack(Stack<MemberInfo>? stack, MemberInfo memberToAppend) {
+            var newStack = new Stack<MemberInfo>(stack?.ToArray() ?? Array.Empty<MemberInfo>());
+            newStack.Push(memberToAppend);
+            return newStack;
+        }
+
+        private static object? AcquireValueByStack(object obj, IEnumerable<MemberInfo>? members) {
+            if (members is null) return obj;
+            var value = obj;
+            foreach (var member in members) member.GetValueImplicitly(value!, out value);
+            return value;
         }
 
         #endregion

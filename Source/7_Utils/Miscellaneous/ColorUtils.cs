37ebb2460c26c21cfbef406e12f09cda0463a105
Hermanest <nicedevelooper@gmail.com>
2023-12-09 19:16:46 +0300

    Added player accent color loading to 'PlayerList'

--- Source/7_Utils/Miscellaneous/ColorUtils.cs
+++ Source/7_Utils/Miscellaneous/ColorUtils.cs
@@ -3,12 +3,17 @@ using Random = System.Random;
 
 namespace BeatLeader.Utils {
     public static class ColorUtils {
-        public static Color RandomColor(int precision = 1000) {
-            var random = new Random();
-            var r = random.Next(0, precision) / (float)precision;
-            var g = random.Next(0, precision) / (float)precision;
-            var b = random.Next(0, precision) / (float)precision;
+        private static readonly Random random = new();
+
+        public static Color RandomColor(int precision = 1000, int startFrom = 300) {
+            var r = Random01(precision, startFrom);
+            var g = Random01(precision, startFrom);
+            var b = Random01(precision, startFrom);
             return new(r, g, b);
+
+            static float Random01(int precision, int startFrom) {
+                return random.Next(startFrom, precision) / (float)precision;
+            }
         }
     }
 }

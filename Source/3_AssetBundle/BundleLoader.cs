242d370aa7eef2c134005f12748a26848aea1037
Hermanest <nicedevelooper@gmail.com>
2023-12-12 21:03:00 +0300

    Changed head model for the battle royale mode

--- Source/3_AssetBundle/BundleLoader.cs
+++ Source/3_AssetBundle/BundleLoader.cs
@@ -6,6 +6,8 @@ using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
+#nullable disable
+
 namespace BeatLeader {
     public static class BundleLoader {
         #region Initialize
@@ -36,16 +38,19 @@ namespace BeatLeader {
 
         #region Prefabs
 
-        public static GameObject MonkeyPrefab;
         public static GameObject AccuracyGraphPrefab;
         public static ThemesCollection ThemesCollection;
+        
         public static GameObject SaberPrefab;
+        public static GameObject HeadPrefab;
+        public static GameObject MonkeyPrefab;
 
         private static void LoadPrefabs(AssetBundle assetBundle) {
             MonkeyPrefab = assetBundle.LoadAsset<GameObject>("TemplatePrefab");
             AccuracyGraphPrefab = assetBundle.LoadAsset<GameObject>("AccuracyGraph");
             ThemesCollection = assetBundle.LoadAsset<ThemesCollection>("ThemesCollection");
             SaberPrefab = assetBundle.LoadAsset<GameObject>("SaberPrefab");
+            HeadPrefab = assetBundle.LoadAsset<GameObject>("HeadPrefab");
         }
 
         #endregion

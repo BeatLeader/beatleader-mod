6c8adb4f6d251e58bd378fd8b97ce8f208c15bff
Hermanest <nicedevelooper@gmail.com>
2024-03-21 18:50:28 +0300

    Fixed replay manager on debug build

--- Source/8_UI/FlowCoordinator/Components/ReplayLaunchView/BeatmapReplayLaunchPanel/ReplaysList.cs
+++ Source/8_UI/FlowCoordinator/Components/ReplayLaunchView/BeatmapReplayLaunchPanel/ReplaysList.cs
@@ -159,16 +159,6 @@ namespace BeatLeader.Components {
 
         [UsedImplicitly]
         private class ReplayDataCell : AbstractDataCell {
-            static ReplayDataCell() {
-                ColorUtility.TryParseHtmlString("#00C0FFFF", out selectColor);
-                highlightSelectedColor = selectColor.ColorWithAlpha(0.75f);
-            }
-
-            private static readonly Color selectColor;
-            private static readonly Color highlightSelectedColor;
-            private static readonly Color highlightColor = Color.white.ColorWithAlpha(0.2f);
-            private static readonly Color idlingColor = Color.clear;
-
             public bool ShowBeatmapName {
                 set {
                     if (ReplayHeader?.ReplayInfo is not { } info) return;
@@ -179,7 +169,23 @@ namespace BeatLeader.Components {
                 }
             }
 
+            private static Color _selectColor;
+            private static Color _highlightSelectedColor;
+            private static Color _highlightColor;
+            private static Color _idlingColor;
+            private static bool _colorsInitialized;
+
+            private void InitColors() {
+                if (_colorsInitialized) return;
+                ColorUtility.TryParseHtmlString("#00C0FFFF", out _selectColor);
+                _highlightSelectedColor = _selectColor.ColorWithAlpha(0.75f);
+                _highlightColor = Color.white.ColorWithAlpha(0.2f);
+                _idlingColor = Color.clear;
+                _colorsInitialized = true;
+            }
+
             protected override void OnConstruct() {
+                InitColors();
                 if (ReplayHeader!.FileStatus is FileStatus.Corrupted
                     || ReplayHeader.ReplayInfo is not { } info) return;
                 ShowBeatmapName = false;
@@ -205,10 +211,10 @@ namespace BeatLeader.Components {
 
             #region Colors
 
-            protected override Color HighlightColor => highlightColor;
-            protected override Color HighlightSelectedColor => highlightSelectedColor;
-            protected override Color SelectColor => selectColor;
-            protected override Color IdlingColor => idlingColor;
+            protected override Color HighlightColor => _highlightColor;
+            protected override Color HighlightSelectedColor => _highlightSelectedColor;
+            protected override Color SelectColor => _selectColor;
+            protected override Color IdlingColor => _idlingColor;
 
             #endregion
         }
@@ -300,11 +306,11 @@ namespace BeatLeader.Components {
         #endregion
 
         #region Data
-        
+
         private IList<IReplayHeader>? _replayHeaders;
 
         public bool showBeatmapNameIfCorrect = true;
-        
+
         public void SetData(IList<IReplayHeader> headers) {
             _replayHeaders = headers;
             Refresh();

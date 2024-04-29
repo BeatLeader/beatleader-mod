5f273c03c38782c19b92234f64ab87977838846e
Hermanest <nicedevelooper@gmail.com>
2024-04-29 16:58:31 +0300

    Added NotifyPropertyChanged support to the basic components.

--- Source/8_UI/Reactive/Components/Image.cs
+++ Source/8_UI/Reactive/Components/Image.cs
@@ -5,54 +5,82 @@ namespace BeatLeader.UI.Reactive.Components {
     internal class Image : DrivingReactiveComponent {
         public Sprite? Sprite {
             get => _image.sprite;
-            set => _image.sprite = value;
+            set {
+                _image.sprite = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public Color Color {
             get => _image.color;
-            set => _image.color = value;
+            set {
+                _image.color = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public Color GradientColor0 {
             get => _image.color0;
-            set => _image.color0 = value;
+            set {
+                _image.color0 = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public Color GradientColor1 {
             get => _image.color1;
-            set => _image.color1 = value;
+            set {
+                _image.color1 = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public bool UseGradient {
             get => _image.gradient;
-            set => _image.gradient = value;
+            set {
+                _image.gradient = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public ImageView.GradientDirection GradientDirection {
             get => _image.GradientDirection;
-            set => _image.GradientDirection = value;
+            set {
+                _image.GradientDirection = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public Material? Material {
             get => _image.material;
-            set => _image.material = value;
+            set {
+                _image.material = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public bool PreserveAspect {
             get => _image.preserveAspect;
-            set => _image.preserveAspect = value;
+            set {
+                _image.preserveAspect = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public UnityEngine.UI.Image.Type ImageType {
             get => _image.type;
-            set => _image.type = value;
+            set {
+                _image.type = value;
+                NotifyPropertyChanged();
+            }
         }
-        
+
         public float PixelsPerUnit {
             get => _image.pixelsPerUnitMultiplier;
             set {
                 ImageType = UnityEngine.UI.Image.Type.Sliced;
                 _image.pixelsPerUnitMultiplier = value;
+                NotifyPropertyChanged();
             }
         }
 

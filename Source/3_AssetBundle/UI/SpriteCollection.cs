34f657ffe6ffea9817ed09f53a8db54ee47a18d3
Hermanest <nicedevelooper@gmail.com>
2024-06-18 20:38:32 +0300

    Added Slider component.

--- Source/3_AssetBundle/UI/SpriteCollection.cs
+++ Source/3_AssetBundle/UI/SpriteCollection.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 namespace BeatLeader {
     [CreateAssetMenu(fileName = "SpriteCollection", menuName = "SpriteCollection")]
     public class SpriteCollection : ScriptableObject {
+        [Space] [Header("Backgrounds")]
         public Sprite rectangle;
         public Sprite background;
+        public Sprite backgroundLeft;
+        public Sprite backgroundRight;
         public Sprite backgroundBottom;
         public Sprite backgroundTop;
+        public Sprite backgroundUnderline;
         /* Icons */
+        [Space] [Header("Icons")]
         public Sprite plusIcon;
         public Sprite minusIcon;
         public Sprite ascendingIcon;
         public Sprite descendingIcon;
+        public Sprite triangleIcon;
         /* Other */
+        [Space] [Header("Misc")]
         public Sprite glare;
+        public Sprite transparentPixel;
     }
 }

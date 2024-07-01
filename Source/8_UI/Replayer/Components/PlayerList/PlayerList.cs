bb6b318006f0b12b798e0685b8bc40494aaa98b5
Hermanest <nicedevelooper@gmail.com>
2024-07-01 14:46:10 +0300

    Fixed conflicts after merge with 'battle-royale-menu-reactive-migration'.

--- Source/8_UI/Replayer/Components/PlayerList/PlayerList.cs
+++ Source/8_UI/Replayer/Components/PlayerList/PlayerList.cs
@@ -188,7 +188,7 @@ namespace BeatLeader.UI.Replayer {
             }
 
             private void RefreshBackgroundMaterial() {
-                var color = Item.Replay.ReplayData.Player!.AccentColor;
+                var color = Item.Replay.OptionalReplayData?.AccentColor ?? Color.clear;
                 _backgroundFillMaterial.SetColor(colorProperty, color);
                 var backgroundColor = color.ColorWithAlpha(0.4f);
                 _backgroundFillMaterial.SetColor(backgroundColorProperty, backgroundColor);

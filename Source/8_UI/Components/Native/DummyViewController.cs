bbec62688a035bd4db3a86ac0028eb86b306c6c5
Hermanest <nicedevelooper@gmail.com>
2024-07-07 15:08:02 +0300

    Fixed beatmap selector causes exception when presented from the solo menu.

--- Source/8_UI/Components/Native/DummyViewController.cs
+++ Source/8_UI/Components/Native/DummyViewController.cs
@@ -16,13 +16,18 @@ namespace BeatLeader.Components {
             _originalViewController = originalViewController;
         }
 
-        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+        public override void __Init(Screen screen, ViewController parentViewController, ContainerViewController containerViewController) {
             if (_originalViewController == null) throw new ArgumentNullException(nameof(_originalViewController));
             _originalScreen = _originalViewController.screen;
             _originalParentController = _originalViewController.parentViewController;
             _originalParent = transform.parent;
             _originalIsInHierarchy = _originalViewController.isInViewControllerHierarchy;
+            base.__Init(screen, parentViewController, containerViewController);
             _originalViewController!.__Init(screen, parentViewController, null);
+        }
+
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+            if (_originalViewController == null) throw new ArgumentNullException(nameof(_originalViewController));
             _originalViewController.__Activate(!_originalIsInHierarchy && addedToHierarchy, screenSystemEnabling);
         }
 
@@ -30,7 +35,12 @@ namespace BeatLeader.Components {
             _originalViewController.SetField("_screen", _originalScreen);
             _originalViewController.SetField("_parentViewController", _originalParentController);
             _originalViewController!.__Deactivate(!_originalIsInHierarchy && removedFromHierarchy, false, screenSystemDisabling);
-            transform.SetParent(_originalParent);
+        }
+
+        public override void __ResetViewController() {
+            _screen = null;
+            transform.SetParent(_originalParent, false);
+            base.__ResetViewController();
         }
     }
 }
--- Source/8_UI/FlowCoordinator/Flows/ReplayManagerFlow/Components/NewFiltersPanel/Filters/BeatmapFilter/LevelSelectionViewController.cs
+++ Source/8_UI/FlowCoordinator/Flows/ReplayManagerFlow/Components/NewFiltersPanel/Filters/BeatmapFilter/LevelSelectionViewController.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using BeatLeader.Components;
 using BeatLeader.UI.Reactive;
+using HarmonyLib;
 using HMUI;
 using IPA.Utilities;
 using UnityEngine.UI;
 using Zenject;
 
 namespace BeatLeader.UI.Hub {
+    [HarmonyPatch]
     internal class LevelSelectionViewController : DummyViewController {
+        #region Patch
+
+        [HarmonyPatch(typeof(LevelCollectionTableView), "SelectLevel"), HarmonyPrefix]
+        private static bool LevelCollectionTablePrefix(IPreviewBeatmapLevel? beatmapLevel) {
+            return beatmapLevel != null;
+        }
+
+        #endregion
+
         #region Injection
 
         [Inject] private readonly LevelSelectionNavigationController _levelSelectionNavigationController = null!;
@@ -26,10 +37,6 @@ namespace BeatLeader.UI.Hub {
 
         #region LevelNavigationController
 
-        private SelectLevelCategoryViewController.LevelCategory _originalLevelCategory = SelectLevelCategoryViewController.LevelCategory.All;
-        private SelectLevelCategoryViewController.LevelCategory _lastSelectedLevelCategory = SelectLevelCategoryViewController.LevelCategory.All;
-        private IPreviewBeatmapLevel? _originalPreviewBeatmapLevel;
-        
         private IconSegmentedControl _levelCategorySegmentedControl = null!;
         private StandardLevelDetailView _levelDetailView = null!;
 
@@ -43,7 +50,7 @@ namespace BeatLeader.UI.Hub {
             //
             _levelCategoryViewController.Setup(levelCategories[0], levelCategories);
             _levelCategorySegmentedControl = _levelCategoryViewController.GetComponentInChildren<IconSegmentedControl>(true);
-            _levelDetailView = _levelDetailViewController.GetField<StandardLevelDetailView, StandardLevelDetailViewController>("_standardLevelDetailView"); 
+            _levelDetailView = _levelDetailViewController.GetField<StandardLevelDetailView, StandardLevelDetailViewController>("_standardLevelDetailView");
             //construct selection detail view
             _selectionDetailView = new() { Enabled = false };
             _selectionDetailView.WithRectExpand().Use(_levelDetailViewController.transform);
@@ -53,7 +60,35 @@ namespace BeatLeader.UI.Hub {
             _patchedClickedEvent.AddListener(HandleActionButtonPressed);
         }
 
-        private void SetupLevelNavigationController() {
+        #endregion
+
+        #region LevelNavigationController
+
+        private SelectLevelCategoryViewController.LevelCategory _lastSelectedLevelCategory = SelectLevelCategoryViewController.LevelCategory.All;
+        private IBeatmapLevelPack? _lastSelectedBeatmapLevelPack;
+
+        private SelectLevelCategoryViewController.LevelCategory _originalLevelCategory;
+        private IPreviewBeatmapLevel? _originalPreviewBeatmapLevel;
+        private IBeatmapLevelPack? _originalBeatmapLevelPack;
+        private BeatmapDifficultyMask _originalAllowedBeatmapDifficultyMask;
+        private BeatmapCharacteristicSO[] _originalNotAllowedCharacteristics = Array.Empty<BeatmapCharacteristicSO>();
+        private bool _originalHidePacksIfOneOrNone;
+        private bool _originalHidePracticeButton;
+        private string _originalActionButtonText = string.Empty;
+
+        private void PatchLevelNavigationController() {
+            //saving initial values
+            _originalLevelCategory = _levelSelectionNavigationController.selectedLevelCategory;
+            _originalPreviewBeatmapLevel = _levelCollectionNavigationController.selectedBeatmapLevel;
+            _originalBeatmapLevelPack = _levelSelectionNavigationController.selectedBeatmapLevelPack;
+            //TODO: asm pub
+            _originalAllowedBeatmapDifficultyMask = _levelSelectionNavigationController.GetField<BeatmapDifficultyMask, LevelSelectionNavigationController>("_allowedBeatmapDifficultyMask");
+            _originalNotAllowedCharacteristics = _levelSelectionNavigationController.GetField<BeatmapCharacteristicSO[], LevelSelectionNavigationController>("_notAllowedCharacteristics");
+            _originalHidePacksIfOneOrNone = _levelSelectionNavigationController.GetField<bool, LevelSelectionNavigationController>("_hidePacksIfOneOrNone");
+            _originalHidePracticeButton = _levelSelectionNavigationController.GetField<bool, LevelSelectionNavigationController>("_hidePracticeButton");
+            _originalActionButtonText = _levelSelectionNavigationController.GetField<string, LevelSelectionNavigationController>("_actionButtonText");
+            //presenting
+            _levelSelectionNavigationController.ClearSelected();
             _levelSelectionNavigationController.Setup(
                 SongPackMask.all,
                 BeatmapDifficultyMask.All,
@@ -61,13 +96,32 @@ namespace BeatLeader.UI.Hub {
                 false,
                 true,
                 "SELECT",
-                null,
-                SelectLevelCategoryViewController.LevelCategory.None,
-                null,
+                _lastSelectedBeatmapLevelPack,
+                _lastSelectedLevelCategory,
+                _originalPreviewBeatmapLevel,
                 true
             );
+            NavigateToBeatmap(null, _lastSelectedLevelCategory);
         }
 
+        private void UnpatchLevelNavigationController() {
+            //restoring initial values
+            _levelSelectionNavigationController.ClearSelected();
+            _levelSelectionNavigationController.Setup(
+                SongPackMask.all,
+                _originalAllowedBeatmapDifficultyMask,
+                Array.Empty<BeatmapCharacteristicSO>(),
+                _originalHidePacksIfOneOrNone,
+                _originalHidePracticeButton,
+                _originalActionButtonText,
+                _originalBeatmapLevelPack,
+                _originalLevelCategory,
+                _originalPreviewBeatmapLevel,
+                true
+            );
+            NavigateToBeatmap(_originalPreviewBeatmapLevel, _originalLevelCategory);
+        }
+        
         #endregion
 
         #region LevelDetail
@@ -75,7 +129,7 @@ namespace BeatLeader.UI.Hub {
         public bool allowDifficultySelection = true;
 
         private LevelSelectionDetailView _selectionDetailView = null!;
-        
+
         private void PatchLevelDetail() {
             if (allowDifficultySelection) return;
             _levelDetailView.gameObject.SetActive(false);
@@ -95,20 +149,15 @@ namespace BeatLeader.UI.Hub {
 
         private Button.ButtonClickedEvent? _lastClickedEvent;
         private Button.ButtonClickedEvent? _patchedClickedEvent;
-        
+
         protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
             _levelDetailViewController.didChangeContentEvent += HandleContentChanged;
             _lastClickedEvent = _levelDetailView.actionButton.onClick;
             _levelDetailView.actionButton.onClick = _patchedClickedEvent;
             //
-            SetupLevelNavigationController();
-            _originalLevelCategory = _levelSelectionNavigationController.selectedLevelCategory;
-            _originalPreviewBeatmapLevel = _levelCollectionNavigationController.selectedBeatmapLevel;
-            NavigateToBeatmap(null, _lastSelectedLevelCategory);
-            base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
-            //
-            _levelCollectionNavigationController.HideDetailViewController();
             PatchLevelDetail();
+            base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
+            PatchLevelNavigationController();
         }
 
         protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling) {
@@ -116,9 +165,14 @@ namespace BeatLeader.UI.Hub {
             _levelDetailView.actionButton.onClick = _lastClickedEvent;
             //
             _lastSelectedLevelCategory = _levelSelectionNavigationController.selectedLevelCategory;
-            NavigateToBeatmap(_originalPreviewBeatmapLevel, _originalLevelCategory);
+            _lastSelectedBeatmapLevelPack = _levelSelectionNavigationController.selectedBeatmapLevelPack;
+            UnpatchLevelNavigationController();
             base.DidDeactivate(removedFromHierarchy, screenSystemDisabling);
+        }
+
+        public override void DeactivateGameObject() {
             UnpatchLevelDetail();
+            base.DeactivateGameObject();
         }
 
         #endregion
@@ -134,9 +188,12 @@ namespace BeatLeader.UI.Hub {
             if (category is SelectLevelCategoryViewController.LevelCategory.None) return;
             //
             var cell = _levelCategorySegmentedControl.cells[(int)category - 1];
-            cell.SetSelected(true, SelectableCell.TransitionType.Instant, cell, false);
+            cell.SetSelected(true, SelectableCell.TransitionType.Instant, cell, true);
             //
-            if (level is null) return;
+            if (level == null) {
+                _levelCollectionNavigationController.HideDetailViewController();
+                return;
+            }
             _levelCollectionNavigationController.SelectLevel(level);
             _levelCollectionNavigationController.HandleLevelCollectionViewControllerDidSelectLevel(null, level);
         }
@@ -156,7 +213,7 @@ namespace BeatLeader.UI.Hub {
             _levelDetailView.gameObject.SetActive(false);
             _selectionDetailView.Refresh();
         }
-        
+
         private void HandleActionButtonPressed() {
             BeatmapSelectedEvent?.Invoke(_levelCollectionNavigationController.selectedDifficultyBeatmap);
         }

6abfbd3587a3777843a714a3a1a88f7c4afe7817
Hermanest <nicedevelooper@gmail.com>
2024-07-14 00:27:33 +0300

    Revert "Fixed leaderboard showing Default Text when pp is zero."
    This reverts commit d51dbe84ce1b6c4afa278e26f8d66fd0dfd8dea6.

--- Source/2_Core/Models/Score.cs
+++ Source/2_Core/Models/Score.cs
@@ -60,7 +60,7 @@ namespace BeatLeader.Models {
                 ScoreRowCellType.Username => true,
                 ScoreRowCellType.Modifiers => true,
                 ScoreRowCellType.Accuracy => true,
-                ScoreRowCellType.PerformancePoints => true,
+                ScoreRowCellType.PerformancePoints => pp > 0,
                 ScoreRowCellType.Score => true,
                 ScoreRowCellType.Mistakes => true,
                 ScoreRowCellType.Clans => true,

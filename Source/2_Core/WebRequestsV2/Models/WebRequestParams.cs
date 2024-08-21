87ea0f48a577c6c798c46026fdb1faa159e1bd26
Hermanest <nicedevelooper@gmail.com>
2024-08-21 13:55:49 +0300

    Added timeout option for web requests.

--- Source/2_Core/WebRequestsV2/Models/WebRequestParams.cs
+++ Source/2_Core/WebRequestsV2/Models/WebRequestParams.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace BeatLeader.WebRequests {
     public class WebRequestParams {
         public float UploadTrackingPrecision { get; set; } = 100f;
         public float DownloadTrackingPrecision { get; set; } = 100f;
+        public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(30);
 
         public float TrackingPrecision {
             set {

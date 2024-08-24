1a025d41c9211c3073a7385e74743c7d10a239b3
Hermanest <nicedevelooper@gmail.com>
2024-08-24 20:38:05 +0300

    Fixed after ModifiersCore refactor.

--- Source/2_Core/Modifiers/SpeedModifiers.cs
+++ Source/2_Core/Modifiers/SpeedModifiers.cs
@@ -5,35 +5,33 @@ using UnityEngine;
 namespace BeatLeader {
     public static class SpeedModifiers {
         public static CustomModifier BFS = new CustomModifier(
-            "BFS", 
+            "BFS",
             "Better Faster Song",
             "Increases song speed by 20%, note speed by 10%",
             BundleLoader.AnchorIcon,
             Color.blue,
             Color.yellow,
             0.1f,
-            new string[] { DefaultCategory.SPEED },
-            new string[] { DefaultCategory.SPEED }
-            );
+            new string[] { ModifierCategories.Speed },
+            new string[] { ModifierCategories.Speed }
+        );
+
         public static CustomModifier BSF = new CustomModifier(
-            "BSF", 
+            "BSF",
             "Better Super Fast Song",
             "Increases song speed by 50%, note speed by 25%",
             BundleLoader.AnchorIcon,
             Color.blue,
             Color.yellow,
             0.2f,
-            new string[] { DefaultCategory.SPEED },
-            new string[] { DefaultCategory.SPEED }
-            );
+            new string[] { ModifierCategories.Speed },
+            new string[] { ModifierCategories.Speed }
+        );
 
         public static float SongSpeed() {
-            if (ModifiersManager.GetModifierState(BFS.Id))
-            {
+            if (ModifiersManager.GetModifierState(BFS.Id)) {
                 return 1.2f;
-            }
-            else if (ModifiersManager.GetModifierState(BSF.Id))
-            {
+            } else if (ModifiersManager.GetModifierState(BSF.Id)) {
                 return 1.5f;
             }
             return 1f;

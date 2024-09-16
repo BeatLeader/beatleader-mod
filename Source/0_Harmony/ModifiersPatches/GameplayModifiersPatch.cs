7b2053eedb8f63a75c5f4900ed4e6e96cf0bd51c
NSGolova <golova@golova.dev>
2024-09-16 19:48:50 +0100

    Fixed default speed modifiers being broken

--- Source/0_Harmony/ModifiersPatches/GameplayModifiersPatch.cs
+++ Source/0_Harmony/ModifiersPatches/GameplayModifiersPatch.cs
@@ -1,27 +1,40 @@
 ﻿using HarmonyLib;
-using IPA.Utilities;
 using JetBrains.Annotations;
 using UnityEngine;
 
 namespace BeatLeader {
     [HarmonyPatch(typeof(GameplayCoreInstaller), "InstallBindings")]
     internal static class GameplayCoreInstallerPatch {
+        internal static GameplayCoreInstaller? instance;
+
         [UsedImplicitly]
         private static void Prefix(GameplayCoreInstaller __instance) {
-            var speed = SpeedModifiers.GetSongSpeed();
-            var modifiers = __instance._sceneSetupData.gameplayModifiers.CopyWith(songSpeed: speed);
-            __instance._sceneSetupData.SetField("gameplayModifiers", modifiers);
+            instance = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(GameplayModifiers), "get_songSpeedMul")]
+    internal static class GameplayModifiersPatch {
+        internal static void Postfix(GameplayModifiers __instance, ref float __result) {
+            if (__instance != GameplayCoreInstallerPatch.instance?._sceneSetupData.gameplayModifiers) return;
+
+            var speed = SpeedModifiers.SongSpeed();
+            if (speed == 1f) return;
+            __result = speed;
         }
     }
 
     [HarmonyPatch(typeof(BeatmapObjectSpawnMovementData), "Init")]
     [HarmonyBefore(new string[] { "com.zephyr.BeatSaber.JDFixer" })]
-    internal class NJSPatch {
+    internal class NJSPatch
+    {
         [UsedImplicitly]
-        internal static void Prefix(ref float startNoteJumpMovementSpeed) {
-            if (BS_Utils.Plugin.LevelData != null && BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData != null) {
-                var speed = SpeedModifiers.GetSongSpeedMultiplier();
-                if (Mathf.Approximately(speed, 1f)) return;
+        internal static void Prefix(ref float startNoteJumpMovementSpeed)
+        {
+            if (BS_Utils.Plugin.LevelData != null && BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData != null)
+            {
+                var speed = SpeedModifiers.SongSpeed();
+                if (speed == 1f) return;
                 // We want to compensate the speed of the AudioTimeSyncController by doing the opposite with the NJS.
                 startNoteJumpMovementSpeed = ((startNoteJumpMovementSpeed * speed - startNoteJumpMovementSpeed) / 2 + startNoteJumpMovementSpeed) / speed;
             }

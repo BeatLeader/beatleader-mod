29a8f94890596445afe7c46bb50d7761224d55f7
Hermanest <nicedevelooper@gmail.com>
2024-11-13 17:08:28 +0300

    Removed the code accidentally added with merge.

--- Source/2_Core/Replayer/Emulation/Scoring/ReplayerScoreProcessor.cs
+++ Source/2_Core/Replayer/Emulation/Scoring/ReplayerScoreProcessor.cs
@@ -24,10 +24,10 @@ namespace BeatLeader.Replayer.Emulation {
         #region Setup
 
         private int _lastEventIndex;
-        
+
         private void Awake() {
             _noteControllerEmulator = new GameObject("NoteControllerEmulator").AddComponent<NoteControllerEmulator>();
-            
+
             _beatmapEventsProcessor.NoteEventDequeuedEvent += HandleNoteBeatmapEventDequeued;
             _beatmapEventsProcessor.WallEventDequeuedEvent += HandleWallBeatmapEventDequeued;
             _beatmapEventsProcessor.EventQueueAdjustStartedEvent += HandleQueueAdjustStarted;
@@ -38,7 +38,7 @@ namespace BeatLeader.Replayer.Emulation {
             _noteWasMissedEnergyCounterPatch.Dispose();
             _finishSwingRatingCounterPatch.Dispose();
             _scoringMultisilencer.Dispose();
-            
+
             _beatmapEventsProcessor.NoteEventDequeuedEvent -= HandleNoteBeatmapEventDequeued;
             _beatmapEventsProcessor.WallEventDequeuedEvent -= HandleWallBeatmapEventDequeued;
             _beatmapEventsProcessor.EventQueueAdjustStartedEvent -= HandleQueueAdjustStarted;
@@ -50,77 +50,6 @@ namespace BeatLeader.Replayer.Emulation {
             var noteCutInfo = noteEvent.noteCutInfo.SaturateNoteCutInfo(noteData!);
             _noteControllerEmulator.Setup(noteData!, noteCutInfo);
             return true;
-        }
-
-        #endregion
-
-        #region NoteData
-
-        private class BeatmapDataItemsComparer : IComparer<BeatmapDataItem> {
-            public int Compare(BeatmapDataItem left, BeatmapDataItem right) {
-                return left.time > right.time ? 1 : left.time < right.time ? -1 : 0;
-            }
-        }
-
-        private static readonly BeatmapDataItemsComparer beatmapItemsComparer = new();
-        private LinkedList<NoteData> _generatedBeatmapNoteData = null!;
-        private LinkedListNode<NoteData>? _startNodeForLastProcessedTime;
-        private IReplayComparator _comparator = null!;
-
-        private bool TryFindNoteData(NoteEvent noteEvent, out NoteData? noteData) {
-            var startNode = _startNodeForLastProcessedTime ?? _generatedBeatmapNoteData.First;
-            var prevNode = _startNodeForLastProcessedTime;
-            for (var node = startNode; node != null; node = node.Next) {
-                noteData = node.Value;
-                if (Mathf.Abs(_startNodeForLastProcessedTime?.Value.time ?? 0 - noteData.time) < 1e-3) {
-                    _startNodeForLastProcessedTime = node;
-                }
-                if (Mathf.Abs(noteEvent.spawnTime - noteData.time) < 1e-3
-                    && _comparator.Compare(noteEvent, noteData)) {
-                    return true;
-                }
-            }
-            _startNodeForLastProcessedTime = prevNode;
-            noteData = null;
-            Plugin.Log.Error("[Replayer] Not found NoteData for id " + noteEvent.noteId);
-            return false;
-        }
-
-        private static IEnumerable<NoteData> CreateSortedNoteDataList(IEnumerable<BeatmapDataItem> items) {
-            var result = new List<NoteData>();
-            foreach (var item in items) {
-                switch (item) {
-                    case NoteData data:
-                        result.Add(data);
-                        break;
-                    case SliderData sliderData:
-                        ConvertAndAddSliderData(sliderData, result);
-                        break;
-                }
-            }
-            result.Sort(beatmapItemsComparer);
-            return result;
-
-            static void ConvertAndAddSliderData(SliderData sliderData, ICollection<NoteData> list) {
-                var sliceCount = sliderData.sliceCount;
-                for (var i = 1; i < sliceCount; ++i) {
-                    var lineIndex = i < sliceCount - 1 ? sliderData.headLineIndex : sliderData.tailLineIndex;
-                    var noteLineLayer = i < sliceCount - 1 ? sliderData.headLineLayer : sliderData.tailLineLayer;
-                    var time = Mathf.LerpUnclamped(sliderData.time, sliderData.tailTime, (float)i / (sliceCount - 1));
-                    var sliderNoteData = NoteData.CreateBurstSliderNoteData(
-                        time,
-                        sliderData.beat,
-                        sliderData.rotation,
-                        lineIndex,
-                        noteLineLayer,
-                        sliderData.headBeforeJumpLineLayer,
-                        sliderData.colorType,
-                        NoteCutDirection.Any,
-                        1f
-                    );
-                    list.Add(sliderNoteData);
-                }
-            }
         }
 
         #endregion
@@ -183,7 +112,7 @@ namespace BeatLeader.Replayer.Emulation {
         #endregion
 
         #region Callbacks
-        
+
         private void HandleNoteBeatmapEventDequeued(LinkedListNode<NoteEvent> noteEventNode) {
             _lastNoteEvent = noteEventNode;
             var noteEvent = noteEventNode.Value;
@@ -292,14 +221,6 @@ namespace BeatLeader.Replayer.Emulation {
                 nameof(NoteWasProcessedPostfix),
                 ReflectionUtils.StaticFlags
             )
-        );
-
-        private readonly HarmonySilencer _cutScoreSpawnerSilencer = new(
-            typeof(NoteCutScoreSpawner).GetMethod(
-                nameof(NoteCutScoreSpawner.HandleScoringForNoteStarted),
-                ReflectionUtils.DefaultFlags
-            )!,
-            false
         );
 
         private readonly HarmonyAutoPatch _finishSwingRatingCounterPatch = new(finishSwingRatingCounterPatchDescriptor);

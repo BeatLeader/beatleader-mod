4f368345eca40fb8da77271127fdd1ba59a25677
NSGolova <golova@golova.dev>
2025-02-23 23:06:08 +0000

    Removed validation bypass

--- Source/2_Core/API/Authentication.cs
+++ Source/2_Core/API/Authentication.cs
@@ -11,14 +11,6 @@ using UnityEngine.Networking;
 
 namespace BeatLeader.API {
 
-    public class CustomCertificateHandler : CertificateHandler
-    {
-        protected override bool ValidateCertificate(byte[] certificateData)
-        {
-            // Bypass validation (not recommended for production)
-            return true;
-        }
-    }
     internal static class Authentication {
         #region AuthPlatform
 
@@ -113,7 +105,6 @@ namespace BeatLeader.API {
             };
 
             var request = UnityWebRequest.Post(BLConstants.SIGNIN_WITH_TICKET, form);
-            request.certificateHandler = new CustomCertificateHandler();
             
             yield return request.SendWebRequest();
 

dd0cde18cec544670bd14b2a7c8d684e8c841da2
NSGolova <golova@golova.dev>
2025-04-27 17:08:52 +0000

    Fixed replayer attempting to load MP environment
    (cherry picked from commit 1c60ea7ad89498292fa8d38898f5d9e3638c2414)

--- Source/2_Core/Replayer/ReplayerMenuLoader.cs
+++ Source/2_Core/Replayer/ReplayerMenuLoader.cs
@@ -307,6 +307,11 @@ namespace BeatLeader.Replayer {
         }
 
         public bool LoadEnvironment(ReplayLaunchData launchData, string environmentName) {
+            if (environmentName == "Multiplayer") { 
+               Plugin.Log.Notice("[ReplayerLoader] Map was played in MP. Skipping \"Multiplayer\" environment");
+               return false; 
+            }
+
             var environment = Resources.FindObjectsOfTypeAll<EnvironmentInfoSO>()
                 .FirstOrDefault(x => x.environmentName == environmentName);
 

cb5c69607ac2d6609e52f1e39c7c370262e9e66e
Reezonate <xxxx359xxxx@gmail.com>
2025-06-08 13:49:07 +0900

    added back text cell alpha fix

--- Source/8_UI/Leaderboard/Components/MainPanel/ScoreRowCells/TextScoreRowCell.cs
+++ Source/8_UI/Leaderboard/Components/MainPanel/ScoreRowCells/TextScoreRowCell.cs
@@ -2,6 +2,7 @@
 using BeatSaberMarkupLanguage.Attributes;
 using JetBrains.Annotations;
 using TMPro;
+using UnityEngine;
 
 namespace BeatLeader.Components {
     internal class TextScoreRowCell : AbstractScoreRowCell {
@@ -42,7 +43,13 @@ namespace BeatLeader.Components {
         }
 
         public override void SetAlpha(float value) {
-            textComponent.alpha = value;
+            if (textComponent.overflowMode == TextOverflowModes.Ellipsis) {
+                var tmp = (Color)textComponent.faceColor;
+                tmp.a = value;
+                textComponent.faceColor = tmp;
+            } else {
+                textComponent.alpha = value;
+            }
         }
 
         protected override float CalculatePreferredWidth() {

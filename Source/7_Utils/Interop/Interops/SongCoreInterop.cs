de5de672e9230052425c2e8f0c5b03fc97c4738c
NSGolova <golova@golova.dev>
2025-10-01 13:18:50 +0000

    Revert "Upgraded SongCore version"
    This reverts commit ef6a51715900d371f16da5917eb04245a67cc0a7.

--- Source/7_Utils/Interop/Interops/SongCoreInterop.cs
+++ Source/7_Utils/Interop/Interops/SongCoreInterop.cs
@@ -10,7 +10,7 @@ namespace BeatLeader.Interop {
         public static bool TryGetBeatmapRequirements(BeatmapLevel beatmap, BeatmapKey key, out string[]? requirements) {
             requirements = null;
             try {
-                var data = SongCore.Collections.GetCustomLevelSongDifficultyData(key);
+                var data = SongCore.Collections.RetrieveDifficultyData(beatmap, key);
                 if (data == null) return false;
                 var reqData = data.additionalDifficultyData;
                 if (reqData == null) return false;

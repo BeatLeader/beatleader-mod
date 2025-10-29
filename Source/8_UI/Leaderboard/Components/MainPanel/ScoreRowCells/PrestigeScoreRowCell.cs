d98ba19ae890035e99545732d5469c259cbcc657
Loloppe <loloppe2020@gmail.com>
2025-10-28 23:42:22 -0400

    Base implementation of Prestige Icon on Score Row Cell

---
+++ Source/8_UI/Leaderboard/Components/MainPanel/ScoreRowCells/PrestigeScoreRowCell.cs
@@ -0,0 +1,39 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using JetBrains.Annotations;
+
+namespace BeatLeader.Components {
+    internal class PrestigeScoreRowCell : AbstractScoreRowCell {
+        #region Components
+
+        private const float Size = 3.0f;
+
+        [UIValue("prestige-icon"), UsedImplicitly]
+        private PrestigeIcon _prestigeIcon;
+        private void Awake() {
+            _prestigeIcon = Instantiate<PrestigeIcon>(transform);
+        }
+
+        #endregion
+
+        #region Implementation
+
+        public override void SetValue(object? value) {
+            if (value is int prestige) {
+                _prestigeIcon.SetPrestige(prestige);
+                isEmpty = false;
+            } else {
+                isEmpty = true;
+            }
+        }
+
+        public override void SetAlpha(float value) {
+            _prestigeIcon.SetAlpha(value);
+        }
+
+        protected override float CalculatePreferredWidth() {
+            return Size;
+        }
+
+        #endregion
+    }
+}

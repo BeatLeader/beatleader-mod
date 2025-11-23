88ae4963c3c2b263c8de7a25dbb1593d3d5d2382
hermanest <nicedevelooper@gmail.com>
2025-11-23 19:18:28 +0300

    Changed foreach to a LINQ expression.

--- Source/8_UI/FlowCoordinator/Navigation/HeckNavigationFlowCoordinator.cs
+++ Source/8_UI/FlowCoordinator/Navigation/HeckNavigationFlowCoordinator.cs
@@ -38,13 +38,11 @@ namespace BeatLeader {
             var type = HeckInterop.SetterViewControllerType!;
             _initMethod = type.GetMethod("Init", ReflectionUtils.DefaultFlags)!;
             _finishedEventField = type.GetField("Finished", ReflectionUtils.DefaultFlags)!;
-            var controllers = _container.ResolveAll(HeckInterop.PlayViewControllerType);
-            foreach (var item in controllers) {
-                if (item is ViewController && item.GetType() == HeckInterop.SetterViewControllerType) {
-                    _setterViewController = (ViewController)item;
-                    break;
-                }
-            }
+            
+            _setterViewController = _container
+                .ResolveAll(HeckInterop.PlayViewControllerType)
+                .Cast<ViewController>()
+                .First(x => x.GetType() == HeckInterop.SetterViewControllerType);
         }
 
         public async Task NavigateToReplayAsync(FlowCoordinator flowCoordinator, Replay replay, Player player, bool tryLoadSelectedMap) {
@@ -97,9 +95,9 @@ namespace BeatLeader {
         }
 
         public void NavigateToBattleRoyale(
-            FlowCoordinator flowCoordinator, 
-            BeatmapLevelWithKey level, 
-            IReadOnlyCollection<IReplayHeader> plays, 
+            FlowCoordinator flowCoordinator,
+            BeatmapLevelWithKey level,
+            IReadOnlyCollection<IReplayHeader> plays,
             bool allowChanges,
             bool clearOnExit
         ) {

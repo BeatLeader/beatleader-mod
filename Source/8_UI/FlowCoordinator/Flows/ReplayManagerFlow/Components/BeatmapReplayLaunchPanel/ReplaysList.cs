88f27c88f5bc61389f0d511349fad1ce8841eaf4
hermanest <mutcmk@gmail.com>
2026-05-14 21:40:17 +0300

    Fixed ReplaysList sorting arrays for too long.

--- Source/8_UI/FlowCoordinator/Flows/ReplayManagerFlow/Components/BeatmapReplayLaunchPanel/ReplaysList.cs
+++ Source/8_UI/FlowCoordinator/Flows/ReplayManagerFlow/Components/BeatmapReplayLaunchPanel/ReplaysList.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using BeatLeader.Models;
-using BeatLeader.Utils;
 using Reactive;
 using Reactive.BeatSaber.Components;
 using Reactive.Components;
@@ -162,16 +162,16 @@ namespace BeatLeader.UI.Hub {
                     if (index == _maxTags) {
                         break;
                     }
-                    
+
                     var panel = _spawnedTags[index];
                     panel.Interactable = false;
                     panel.AnimateMovement = false;
                     panel.SetTag(tag);
-                    
+
                     panel.FixedHeight = 4f;
                     panel.LayoutDriver = _tagsContainer;
                     panel.RefreshContainerSize(true);
-                    
+
                     index++;
                 }
             }
@@ -192,7 +192,7 @@ namespace BeatLeader.UI.Hub {
                             if (_spawnedTags.Count == _maxTags) {
                                 break;
                             }
-                            
+
                             var tag = _replaysList!._tagsPool.Spawn();
                             _spawnedTags.Add(tag);
                         }
@@ -340,24 +340,27 @@ namespace BeatLeader.UI.Hub {
 
         #region Sorting
 
-        private class HeaderComparator : IComparer<IReplayHeader> {
+        private class HeaderComparator : IComparer {
             public ReplaysListSorter sorter;
 
-            public int Compare(IReplayHeader x, IReplayHeader y) {
-                var xi = x.ReplayInfo;
-                var yi = y.ReplayInfo;
+            public int Compare(object? x, object? y) {
+                if (x is not IReplayHeader { ReplayInfo: var xi }) {
+                    return 0;
+                }
+
+                if (y is not IReplayHeader { ReplayInfo: var yi }) {
+                    return 0;
+                }
+
                 return sorter switch {
                     ReplaysListSorter.Difficulty =>
                         -CompareLong(
                             (int)Enum.Parse(typeof(BeatmapDifficulty), xi.SongDifficulty),
                             (int)Enum.Parse(typeof(BeatmapDifficulty), yi.SongDifficulty)
                         ),
-                    ReplaysListSorter.Player =>
-                        string.CompareOrdinal(xi.PlayerName, yi.PlayerName),
-                    ReplaysListSorter.Completion =>
-                        CompareLong((int)xi.LevelEndType, (int)yi.LevelEndType),
-                    ReplaysListSorter.Date =>
-                        -CompareLong(xi.Timestamp, yi.Timestamp),
+                    ReplaysListSorter.Player => string.CompareOrdinal(xi.PlayerName, yi.PlayerName),
+                    ReplaysListSorter.Completion => CompareLong((int)xi.LevelEndType, (int)yi.LevelEndType),
+                    ReplaysListSorter.Date => -CompareLong(xi.Timestamp, yi.Timestamp),
                     _ => throw new ArgumentOutOfRangeException()
                 };
             }
@@ -387,8 +390,13 @@ namespace BeatLeader.UI.Hub {
         private SortOrder _sortOrder;
 
         public void Sort() {
-            Items.Sort(_headerComparator);
-            if (_sortOrder is SortOrder.Descending) Items.Reverse();
+            var arrayList = ArrayList.Adapter((IList)Items);
+            
+            arrayList.Sort(_headerComparator);
+
+            if (_sortOrder is SortOrder.Descending) {
+                arrayList.Reverse();
+            }
         }
 
         #endregion
